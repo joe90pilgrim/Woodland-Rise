a8d4d32c4922a46412d5f3bfe2bf40c9990a8f63
joe90pilgrim <joe90pilgrim@gmail.com>
2023-07-01 14:30:39 +0100

    Switched to Contentful image loading

--- src/app/external-services/contentful/contentful.service.ts
+++ src/app/external-services/contentful/contentful.service.ts
@@ -23,6 +23,12 @@ export class ContentfulService {
     return observableFromPromise;
   }
 
+  getAsset(contentId) {
+    const promise = this.client.getAsset(contentId);
+    var observableFromPromise = fromPromise(promise);
+    return observableFromPromise;
+  }
+
   // convert markdown string to 
   markdownToHtml(md: string) {
     return marked(md)
@@ -39,6 +45,37 @@ export class ContentfulService {
         break;
       case 'bookingLink':
         contentId = "6WsVV03eiDisab5Zsh76UZ"
+        break;
+      case 'image1':
+        contentId = '5Tt0dFb0bv3hh43RIGB7nj'
+        break;
+      case 'image2':
+        contentId = '7dz3aItlOqDlm3Hs0j8AIa'
+        break;
+      case 'image3':
+        contentId = '4WoIpvvNWOhacz9ukmhcsT'
+        break;
+      case 'image4':
+        contentId = '3PaJ5AaE0zVsZwgH9Hzi7d'
+        break;
+      case 'image5':
+        contentId = '701a7zcgRfRulEXEQmPgqI'
+        break;
+      case 'image6':
+        contentId = '6TMxDdO5U90hgbfod6wCUO'
+        break;
+      case 'image7':
+        contentId = '2OijwX6Zdrrr4Q0c9Cr4Ml'
+        break;
+      case 'image8':
+        contentId = '55BM4nrFvXhQfDsX0NVDRS'
+        break;
+      case 'image9':
+          contentId = '5Lln0Z5w3gcIIQ5sZwq4tG'
+          break;
+      case 'image10':
+        contentId = '4cURafhsaQLC8PfG6hTYuG'
+        break;
     }
     return contentId;
   }
--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ContentfulService } from '../external-services/contentful/contentful.service';
 
 @Component({
   selector: 'app-gallery',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent implements OnInit {
+  image1url = null;
+  image2url = null;
+  image3url = null;
+  image4url = null;
+  image5url = null;
+  image6url = null;
+  image7url = null;
+  image8url = null;
+  image9url = null;
+  image10url = null;
 
-  constructor() { }
+  imageUrls = []
+  images = ['image1','image2','image3','image4','image5','image6','image7','image8','image9','image10',]
+
+  constructor(private content: ContentfulService) { }
 
   ngOnInit(): void {
+    for (let img in this.images) {
+      this.getContentfulContent(this.images[img]);
+    }    
+  }
+
+  getContentfulContent(content) {
+    let contentToFind = this.content.getContentId(content)
+    this.content.getAsset(contentToFind).subscribe(response => {
+      this[content + 'url'] = response.fields.file.url;
+    });
   }
 
   
